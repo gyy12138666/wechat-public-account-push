8a357ee9ffc646915e77252a9044f28fde749454
Gong YiYuan <1635974992@qq.com>
2025-05-20 00:43:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o6I3a7V4vD8DOZWu7a2YAuE3tXXU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'AsVxOVGNICSdUrsWqm_4annUahYuOYu5GYX1d09bheo',
+      useTemplateId: 'nU5Kl1syUgAp5YCkSqen2ZZgNsMyMQj_t5gcUVQou1o',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-23',
       festivals: [
