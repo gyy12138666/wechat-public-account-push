9143814db09d0bc3ce70614a1ba0dabfca29e2e3
Gong YiYuan <1635974992@qq.com>
2025-05-22 15:18:50 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -109,7 +109,7 @@
       id: 'o6I3a7V4vD8DOZWu7a2YAuE3tXXU',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       // 使用其他通道时，请填写 config/template-config.cjs 中某个想要使用的模板的id
-      useTemplateId: 'dYnSICX_RJQgkM0Mq_zUFCHPR_UNksHdlzmXvHE1-ug',
+      useTemplateId: 'cVGGMGhoWwFQ34i1hxGKQL6a0uhZUjX35VWQccx2D5Y',
       // 所在省份或城市，也可以不填
       province: '广西',
       // 所在城市或县区
